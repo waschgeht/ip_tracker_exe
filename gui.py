939651c654c4237369f2365b5137193449553203
waschgeht <ainetter.martin@gmail.com>
2020-12-25 19:12:34 +0100

    25.12.2020 19:12 Created new updatable .exe version!

--- gui.py
+++ gui.py
@@ -1,40 +1,81 @@
 import tkinter as tk
+from urllib.request import urlretrieve
+from tkinter import ttk
+import time
+from os import path
+from smtplib import SMTP_SSL
 from _datetime import datetime
-from subprocess import PIPE, Popen
+from base64 import b64encode, b64decode
+from subprocess import Popen, PIPE
 
-
-#umgehe noconsole error
+'''umgehe noconsole error bei convert durch pyinstaller'''
 def cmd(command):
     process = Popen(command, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     ip = process.communicate()
     return ip
 
+def new_update():
+    try:
+        newest_version = float(cmd("curl --max-time 1 -s http://softwareupdt.duckdns.org:8080/ip_tracker/version.txt")[0].decode('ascii'))
+        if newest_version > 0.1:
+            return True
+        else:
+            return False
+    except Exception as error:
+        logging("failed update check; " , error)
 
 '''funktion requestet ip von ifconfig.me'''
 def external_ip_requester():
     try:
-        ip = str(cmd("curl -s http://ifconfig.me/ip")[0]) #.popen erzeugt keine Ausgabe am Screen
+        ip = str(cmd("curl -s http://ifconfig.me/ip")[0]) #self made function cmd because noconsole error
         ip= ip[2:len(ip)-1]
         int(ip[0]) #testet ob ausgabe eine IP ist. Erzeugt fehler wenn falsch und läuft in except block
         return ip
     except:
-        print("Couldnt get ip from http://ifconfig.me/ip")
+        logging("Couldnt get ip from http://ifconfig.me/ip")
         raise SystemExit(0) #schließt das programm komplett
 
+'''Base 64 encodes and decodes messages'''
+def bencode(secret):
+    return b64encode(bytes(secret, encoding='utf-8')).decode('ascii')
 
+def bdecode(secret):
+    return b64decode(secret).decode('ascii')
+
+'''
+Sendet plain text message
+'''
+def Pfad():
+    return "C:\\ip_tracker"
+
+def send_text():
+    Path = Pfad()
+    with open(Path + "\\data.conf", "r") as data: #liest daten aus .conf file; Positionsabhängig!!!
+        Data = data.readlines()
+        for i in range(0,3):
+            Data[i] = bdecode(Data[i])
+    try:
+        message = 'Subject: {}\n\n{}'.format("Your current IP", "Your current IP is " + Data[3]) #erstellt message mit Data[2]=IP
+        server = SMTP_SSL("smtp.gmail.com", 465) #funktionierrt nur für gmail server. Applikationszugriff uss aktiviert sein
+        server.login(Data[0], Data[1])  #Data[0]=your email; Data[1]=your Email password
+        server.sendmail(Data[0], Data[2], message) #Data[0]=your email; Data[2]=receiver email
+        server.quit()
+        logging("Email was sent")
+    except Exception as error:
+        logging("Couldn't send email;   " + str(error))
 
 def logging(TEXT):
-    Path = "C:\\ip_tracker"
+    Path = Pfad()
     file_location = Path + "\\email.log"
     date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     try:
         with open(file_location, "a+") as log:
             log.write(date + ";   " + TEXT + " \n")
     except Exception as nopen1:
-        print("Couldn't write to file")
-        print(nopen1)
+        pass
 
 def schedule_task(frequency, time):
+    Path = Pfad()
     try:
         Tasks = str(cmd('SCHTASKS'))
         Tasks.index("ip_tracker")
@@ -45,7 +86,7 @@ def schedule_task(frequency, time):
     except:
         pass
     try:
-        cmd('SCHTASKS /CREATE /SC ' + str(frequency) + ' /TN "ip_tracker" /TR "C:\\ip_tracker\\main.exe" /ST ' + str(time))
+        cmd('SCHTASKS /CREATE /SC ' + str(frequency) + ' /TN "ip_tracker" /TR "' + Path + '\\main.exe" /ST ' + str(time))
         logging("Task wurde erstellt")
     except Exception as error:
         logging("Fehler beim erstellen von Task; ", error)
@@ -57,15 +98,17 @@ def  disable_task():
     except Exception as error:
         logging("Couldn't disable task; ", error)
 
+def enable_task():
+    try:
+        cmd('SCHTASKS /CHANGE /TN "ip_tracker" /ENABLE')
+        logging("Task enabled")
+    except Exception as error:
+        logging("Couldn't enable task; ", error)
 
 def WriteToFile(email, password, receiver, ip):
-    Path = "C:\\ip_tracker"
+    Path = Pfad()
     with open(Path + "\\data.conf", "w+") as file:
         file.writelines([email, "\n", password, "\n", receiver, "\n", ip])
-
-
-
-
 
 def CancelButton():
     root.destroy()
@@ -74,12 +117,31 @@ def ApplyButton():
     if EnableDiable.get()=="disable":
         disable_task()
     elif EnableDiable.get()=="enable":
-        WriteToFile(e1.get(),e2.get(), e3.get(), external_ip_requester())
+        WriteToFile(bencode(e1.get()),bencode(e2.get()), bencode(e3.get()), external_ip_requester())
         schedule_task(frequence.get(), e4.get())
+        send_text()
+    root.destroy()
+
+def bar_update(bar, Wert):
+    bar['value'] = Wert
+    root.update_idletasks()
+    time.sleep(1)
+
+def download():
+    bar = ttk.Progressbar(root, orient="horizontal", length=200)
+    bar.grid(row=2, columnspan=2, padx=30, pady=20)
+    bar_update(bar, 0)
+    urlretrieve("http://softwareupdt.duckdns.org:8080/ip_tracker/gui.exe", "gui.exe")
+    bar_update(bar, 50)
+    urlretrieve("http://softwareupdt.duckdns.org:8080/ip_tracker/main.exe", "main.exe")
+    bar_update(bar, 100)
     root.destroy()
 
 try:
     root = tk.Tk()
+    #photo = tk.PhotoImage(file=Pfad() + "\\icon1.png")
+    #root.iconphoto(False, photo)
+    root.title("IP tracker")  # titel
     tk.Label(root, text="Please be aware, for this to work you need to \n enable less safer apps on gmail!\n To do so please follow this link: \n \n https://myaccount.google.com/lesssecureapps \n").pack()
     tk.Button(root, text="Ok", command=root.destroy).pack()
     root.mainloop()
@@ -87,28 +149,47 @@ except:
     root.destroy()
     logging("Startwindow failure")
 
+try:
+    if new_update():
+        try:
+            root = tk.Tk()
+            #photo = tk.PhotoImage(file=Pfad() + "\\icon1.png")
+            #root.iconphoto(False, photo)
+            root.title("Update available!")  # titel
+            tk.Label(root, text="There is a new update available, \n do you want to download the newest .exe version?").grid(row=0, columnspan=2, padx=10, pady=10)
+            tk.Button(root, text="   Yes   ", command=download).grid(row=1, column=0,padx=10, pady=10, sticky=tk.E)
+            tk.Button(root, text="   No   ", command=root.destroy).grid(row=1, column=1,padx=10, pady=10, sticky=tk.W)
+            tk.Label(root, text="     ").grid(row=2, columnspan=2, padx=30, pady=20)
+            root.mainloop()
+        except:
+            root.destroy()
+            logging("Updatewindow failure")
+except:
+    logging("Update failure")
 
 '''Gui buit up with grid'''
 try:
     global e1, e2, e3
     root = tk.Tk()
+    #photo = tk.PhotoImage(file=Pfad() + "\\icon1.png")
+    #root.iconphoto(False, photo)
     root.title("IP tracker") #titel
     tk.Label(root, text="Your Email").grid(row=0) #Label with Grid
     tk.Label(root, text="Your Password").grid(row=1)
     tk.Label(root, text="Receiver Email").grid(row=0, column=3)
     tk.Label(root, text="Frequency").grid(row=2, pady=(30,0))
     tk.Label(root, text="Enable/Disable").grid(row=2, column=4, pady=(30,0))
-    tk.Label(root, text="Starttime").grid(row=2, column=1, pady=(30,0))
+    tk.Label(root, text="Starttime").grid(row=2, column=1, pady=(30,0), sticky=tk.W)
 
-    e1 = tk.Entry(root) #Eingabefelder Email
-    e2 = tk.Entry(root) #Enter password
-    e3 = tk.Entry(root) #Enter receiver
+    e1 = tk.Entry(root, width=40) #Eingabefelder Email
+    e2 = tk.Entry(root, show="*", width=40) #Enter password
+    e3 = tk.Entry(root, width=40) #Enter receiver
     e4 = tk.Entry(root)  # Enter Starttime (Format 17:30)
 
     e1.grid(row=0, column=1, padx=10, pady=10) #Padding applies to both sides x for x axis y for y axis
     e2.grid(row=1, column=1, padx=10, pady=10)
     e3.grid(row=0, column=4,padx=10, pady=10)
-    e4.grid(row=3, column=1, padx=10, pady=10)
+    e4.grid(row=3, column=1, padx=10, pady=10, sticky=tk.W)
 
     frequence = tk.StringVar(root) #Variable for drop down menue
     frequence.set("hourly")  # default value
@@ -118,7 +199,7 @@ try:
     EnableDiable.set("enable")  # default value
     Enabler = tk.OptionMenu(root, EnableDiable, "enable", "disable")  # Dropdown choices
     Enabler.grid(row=3, column=4, padx=10)
-    print(EnableDiable.get())
+
     '''This function toggles the entry widgets away is the ip_tracker should be disabled'''
     def toogle(*args):
         global e1,e2, e3
--- main.py
+++ main.py
@@ -1,29 +1,52 @@
 from sys import exit
-from subprocess import Popen, PIPE
 from smtplib import SMTP_SSL
 from _datetime import datetime
+from base64 import b64encode, b64decode
+from subprocess import Popen, PIPE
 
-'''funktion requestet ip von ifconfig.me'''
+def Pfad():
+    return "C:\\ip_tracker"
 
+'''umgehe noconsole error bei convert durch pyinstaller'''
 def cmd(command):
     process = Popen(command, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     ip = process.communicate()
     return ip
 
+'''Base 64 encodes and decodes messages'''
+def bencode(secret):
+    return b64encode(bytes(secret, encoding='utf-8')).decode('ascii')
+
+def bdecode(secret):
+    return b64decode(secret).decode('ascii')
+
+def logging(TEXT):
+    Path = Pfad()
+    file_location = Path + "\\email.log"
+    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    try:
+        with open(file_location, "a+") as log:
+            log.write(date + ";   " + TEXT + " \n")
+    except Exception as nopen1:
+        pass
+
+'''funktion requestet ip von ifconfig.me'''
 def external_ip_requester():
     try:
-        ip = str(cmd("curl -s http://ifconfig.me/ip")[0]) #.popen erzeugt keine Ausgabe am Screen
+        ip = str(cmd("curl -s http://ifconfig.me/ip")[0]) #self made function cmd because noconsole error
         ip= ip[2:len(ip)-1]
         int(ip[0]) #testet ob ausgabe eine IP ist. Erzeugt fehler wenn falsch und läuft in except block
         return ip
     except:
-        print("Couldnt get ip from http://ifconfig.me/ip")
+        logging("Couldnt get ip from http://ifconfig.me/ip")
         raise SystemExit(0) #schließt das programm komplett
 
 def send_text():
-    Path = "C:\\ip_tracker"
+    Path = Pfad()
     with open(Path + "\\data.conf", "r") as data: #liest daten aus .conf file; Positionsabhängig!!!
         Data = data.readlines()
+        for i in range(0,3):
+            Data[i] = bdecode(Data[i])
     try:
         message = 'Subject: {}\n\n{}'.format("Your current IP", "Your current IP is " + Data[3]) #erstellt message mit Data[2]=IP
         server = SMTP_SSL("smtp.gmail.com", 465) #funktionierrt nur für gmail server. Applikationszugriff uss aktiviert sein
@@ -32,36 +55,21 @@ def send_text():
         server.quit()
         logging("Email was sent")
     except Exception as error:
-        print("Error, couldn't send message")
-        print(error)
         logging("Couldn't send email;   " + str(error))
-
-def logging(TEXT):
-    Path = "C:\\ip_tracker"
-    file_location = Path + "\\email.log"
-    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    try:
-        with open(file_location, "a+") as log:
-            log.write(date + ";   " + TEXT + " \n")
-    except Exception as nopen1:
-        print("Couldn't write to file")
-        print(nopen1)
-
-
 
 
 '''Main function thats gonna be startet by the schedules job!'''
 def Main():
-    Path = "C:\\ip_tracker"
+    Path = Pfad()
     ip = external_ip_requester() #Requests email from ifconfig.me/ip
-    with open(Path + "\\data.conf", "r+") as data: #Ließt Daten ein und ändert nur Eintrag 4 (Ip)
+    with open(Path + "\\data.conf", "r") as data: #Ließt Daten ein und ändert nur Eintrag 4 (Ip)
         ReadData = data.readlines()
         if str(ReadData[3]) == str(ip):
             exit()
         else:
             ReadData[3] = str(ip)
 
-    with open(Path + "\\data.conf", "w+") as Data: #Schreibt änderungen zum File
+    with open(Path + "\\data.conf", "w") as Data: #Schreibt änderungen zum File
         Data.writelines(ReadData)
         Data.close()
     send_text()
